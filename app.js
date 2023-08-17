f80400a04f8ca84847b5657ada0809f7ec5588e0
Coded-Eye <reliczexide@gmail.com>
2023-08-18 00:53:49 +0100

    commit

---
+++ app.js
@@ -0,0 +1,52 @@
+let x = undefined
+let y = undefined
+
+let myInterval = undefined
+
+document.body.addEventListener('click', (e) => {
+
+    if (!document.querySelector('.autoscrollCursorManager')) {
+        if (e.ctrlKey && e.button === 0) {
+
+            // Create autoscrollCursorManager
+            const div = document.createElement('div');
+            
+            div.classList.add('autoscrollCursorManager')
+
+            // Set x and y
+            x = e.clientX - 15
+            y = e.clientY - 15
+
+            // move it to the x and y
+            div.style.left = `${x}px`
+            div.style.top = `${y}px`
+
+            // Add cursor to the div
+            document.body.appendChild(div)
+            
+            myInterval = setInterval(() => {
+                this.scrollBy({
+                    top: y - e.clientY + 15,
+                    left: x - e.clientX + 15,
+                    behavior: "smooth",
+                })
+            }, 10)
+
+            document.body.style.cursor = "all-scroll"
+        }
+
+    } else {
+        e.preventDefault()
+        document.querySelector('.autoscrollCursorManager').remove()
+        x = undefined
+        y = undefined
+
+        clearInterval(myInterval)
+        document.body.style.cursor = "default"
+    }
+})
+
+document.addEventListener('mousemove', (e) => {
+    x = e.clientX - 15
+    y = e.clientY - 15
+})
